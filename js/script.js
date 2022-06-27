b4f00b71f3f7dda74ec87619535426dfcc79821a
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2022-06-27 12:17:42 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-alert('Hello world');
-
-favoriteFood = 'Burgers';
-document.write(favoriteFood);
+let pokemonList = [
+//My 6 Starters always when playing pokemon growing up
+  {name:'Charizard', height:5.6, type:['fire', 'flying']},
+  {name:'Alakazam', height:4.9, type:['psychic']},
+  {name:'Dragonite', height:7.2, type:['dragon', 'flying']},
+  {name:'Scyther', height:4.9, type:['bug', 'flying']}.
+  {name:'Gyarados', height:21.3, type:['water', 'flying']},
+  {name:'Jolteon', height:2.6, type:['electric']},
+//Honorable mentions:gengar, lapras, articuno, zapdos,
+//mewtwo, raichu, pidgeot.
+];
