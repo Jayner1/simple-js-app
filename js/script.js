80221de17be07a3904644d24d296cb5f226fb13b
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2022-07-20 09:50:27 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 let pokemonRepository = (function () {
   let repository = [
 //My 6 Starters always when playing pokemon growing up
-  {name:"Charizard", height:5.6, type:["fire", "flying"]},
-  {name:"Alakazam", height:4.9, type:["psychic"]},
-  {name:"Dragonite", height:7.2, type:["dragon", "flying"]},
-  {name:"Scyther", height:4.9, type:["bug", "flying"]},
-  {name:"Gyarados", height:21.3, type:["water", "flying"]},
-  {name:"Jolteon", height:2.6, type:["electric"]},
+  {name:"Charizard", height:5.6, types:["fire", "flying"],},
+  {name:"Alakazam", height:4.9, types:["psychic"],},
+  {name:"Dragonite", height:7.2, types:["dragon", "flying"],},
+  {name:"Scyther", height:4.9, types:["bug", "flying"],},
+  {name:"Gyarados", height:21.3, types:["water", "flying"],},
+  {name:"Jolteon", height:2.6, types:["electric"],},
 //Honorable mentions:gengar, lapras, articuno, zapdos,
 //mewtwo, raichu, pidgeot.
+
 ];
 	function add(pokemon) {
     if (
@@ -35,13 +36,13 @@ function addListItem(pokemon){
     pokemonList.appendChild(listpokemon);
     button.addEventListener("click", function() {
       showDetails(pokemon);
-  }
+  });
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem
       };
-    })();
+  }
 
 console.log(pokemonRepository.getAll());
 
